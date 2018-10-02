5009a11a77152195637908f160951d92d85a48d9
Jibin <jibinmathews7@gmail.com>
2018-10-02 11:11:18 +0530

    Adding helm chart

---
+++ .circleci/helm-apply.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+. ./.circleci/export-env-vars.sh
+
+aws s3 cp s3://bc-kubeconfig/config ~/.kube/config
+
+helm init --client-only
+
+setVariables="NODE_ENV=${NODE_ENV},image=${IMAGE_NAME},commitHash=${COMMIT_HASH},ROOT_URL=${ROOT_URL},,COMMIT_HASH=${COMMIT_HASH}"
+releaseName="enterprise-app-${NODE_ENV}"
+
+echo $setVariables
+
+helm --debug \
+  --kube-context "k8s-${CLUSTER_PREFIX}.blockcluster.io" \
+  upgrade \
+  --install \
+  --set ${setVariables} \
+  --namespace ${NODE_ENV} \
+  $releaseName \
+  ./helm-chart
+
