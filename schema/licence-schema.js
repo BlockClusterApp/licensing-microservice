067c436c386d278ef38f09b69ae4a176e86617b0
saikat <saikatchakrabortty2@gmail.com>
2018-10-06 12:47:18 +0530

    Schema updated

--- schema/licence-schema.js
+++ schema/licence-schema.js
@@ -1,31 +1,37 @@
 const mongoose = require('mongoose');
 
-const LicenseSchema = new mongoose.Schema({
-  clientId: {
-    type: Number,
-  },
-  createdAt: {
-    type: Date,
-  },
-  licenseDetails: {
-    type: {
-      licenseKey: String,
-      licenseCreated: Date,
-      licenseExpiry: Date,
+const LicenseSchema = new mongoose.Schema(
+  {
+    clientId: {
+      type: Number,
+    },
+    licenseDetails: {
+      type: {
+        licenseKey: String,
+        licenseCreated: Date,
+        licenseExpiry: Date,
+      },
+    },
+    access_key: {
+      type: String,
+      select: false,
+    },
+    clientDetails: {
+      type: {
+        clientName: String,
+        emailId: String,
+        phone: String,
+      },
+    },
+    status: {
+      type: Boolean,
     },
   },
-  access_key: String,
-  clientDetails: {
-    type: {
-      clientName: String,
-      emailId: String,
-      phone: String,
-    },
-  },
-  status: {
-    type: Boolean,
-  },
-});
+  {
+    collection: 'clients',
+    timestamps: true,
+  }
+);
 
 const LicenceModel = mongoose.model('licence', LicenseSchema);
 
