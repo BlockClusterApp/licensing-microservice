d0589184a5a103b186ed3334f8acbd0c95516232
saikat <saikatchakrabortty2@gmail.com>
2018-10-16 09:58:36 +0530

    run this in all ENV now

--- .circleci/export-env-vars.sh
+++ .circleci/export-env-vars.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 export COMMIT_HASH=${CIRCLE_SHA1}
-# if [ "$CIRCLE_TAG" = "production" ] || [ "$CIRCLE_BRANCH" = "master" ] || [ "$CIRCLE_BRANCH" = "hot-fix" ];
-# then
-#   export NODE_ENV=production
-#   export CLUSTER_PREFIX="production-ap-south-1b";
-# elif [ "$CIRCLE_TAG" = "staging" ] || [ "$CIRCLE_BRANCH"  = "staging" ];
-# then
-#   export NODE_ENV=staging
-#   export CLUSTER_PREFIX="dev";
-# elif [ "$CIRCLE_TAG" = "test" ] || [ "$CIRCLE_BRANCH" = "test" ] || [ "$IS_TEST" = "1" ];
-# then
-#   export NODE_ENV=test
-#   export CLUSTER_PREFIX="dev";
-# elif [ "$CIRCLE_TAG" = "dev" ] ||  [ "$CIRCLE_BRANCH" = "dev" ];
-# then
+if [ "$CIRCLE_TAG" = "production" ] || [ "$CIRCLE_BRANCH" = "master" ] || [ "$CIRCLE_BRANCH" = "hot-fix" ];
+then
+  export NODE_ENV=production
+  export CLUSTER_PREFIX="production-ap-south-1b";
+elif [ "$CIRCLE_TAG" = "staging" ] || [ "$CIRCLE_BRANCH"  = "staging" ];
+then
+  export NODE_ENV=staging
+  export CLUSTER_PREFIX="dev";
+elif [ "$CIRCLE_TAG" = "test" ] || [ "$CIRCLE_BRANCH" = "test" ] || [ "$IS_TEST" = "1" ];
+then
+  export NODE_ENV=test
+  export CLUSTER_PREFIX="dev";
+elif [ "$CIRCLE_TAG" = "dev" ] ||  [ "$CIRCLE_BRANCH" = "dev" ];
+then
   export NODE_ENV=dev
   export CLUSTER_PREFIX="dev";
-# fi
+fi
 
 
 export IMAGE_NAME='402432300121.dkr.ecr.us-west-2.amazonaws.com/licensing-micro'
