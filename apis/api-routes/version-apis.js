36ede0cb273a2220fb485a771e58025e547aa74a
saikat <saikatchakrabortty2@gmail.com>
2018-10-18 14:41:55 +0530

    Small bug fix

--- apis/api-routes/version-apis.js
+++ apis/api-routes/version-apis.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const versionController = require('../controllers/version_save');
 
 router.post('/takeIn', (req, res, next) => {
-  if (req.body.version && req.body.app) {
+  if (!req.body.version || !req.body.app) {
     return next({
       status: 400,
       message: 'version & app required',
