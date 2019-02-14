f8f1f3821ea99ffdf0cf6d254d2d8fac62c1d87b
Jibin Mathews <jazzyarchitects@users.noreply.github.com>
2019-02-14 19:30:54 +0530

    Changing deploy to DO cluster in dev

--- .circleci/helm-apply.sh
+++ .circleci/helm-apply.sh
@@ -2,7 +2,13 @@
 
 . ./.circleci/export-env-vars.sh
 
-aws s3 cp s3://bc-kubeconfig/config ~/.kube/config
+if [ "$NODE_ENV" = "production" ]; then
+  aws s3 cp s3://bc-kubeconfig/config ~/.kube/config
+  export KUBECONTEXT="k8s-${CLUSTER_PREFIX}.blockcluster.io";
+else
+  aws s3 cp s3://bc-kubeconfig/k8s-dev-do.blockcluster.io.yaml ~/.kube/config
+  export KUBECONTEXT="do-lon1-do-dev-blockclusterio"
+fi
 
 helm init --client-only
 
@@ -10,7 +16,7 @@ setVariables="NODE_ENV=${NODE_ENV},image=${IMAGE_NAME},commitHash=${COMMIT_HASH}
 releaseName="enterprise-app-${NODE_ENV}"
 
 helm --debug \
-  --kube-context "k8s-${CLUSTER_PREFIX}.blockcluster.io" \
+  --kube-context "$KUBECONTEXT" \
   upgrade \
   --install \
   --set ${setVariables} \
